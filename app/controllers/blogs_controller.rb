3a407163ffd90adfbca1d38c9297a3ad661a7874
BAROKA <birotori@gmail.com>
2020-08-05 14:03:50 +0100

    edit function & destroy

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,4 +1,5 @@
 class BlogsController < ApplicationController
+  before_action :set_blog, only: [:show, :edit, :update, :destroy]
   def index
     @blog=Blog.all
   end
@@ -17,12 +18,31 @@ class BlogsController < ApplicationController
   end
 
  def show
-   @blog=Blog.find(params[:id])
+   #@blog=Blog.find(params[:id])
  end
 
+ def edit
+   #@blog=Blog.find(params[:id])
+ end
 
+ def update
+   #@blog=Blog.find(params[:id])
+   if @blog.update(blog_params)
+      redirect_to blogs_path, notice: "Post was successfully update."
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @blog.destroy
+    redirect_to blogs_path, notice: "Post was successfully destroy."
+  end
   private
   def blog_params
     params.require(:blog).permit(:content)
+  end
+  def set_blog
+    @blog=Blog.find(params[:id])
   end
 end
